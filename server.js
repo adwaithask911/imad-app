87a4dc88c924582221f290be5cbb94da2645d070
adwaithask911 <30690789+adwaithask911@users.noreply.github.com>
2017-08-06 09:11:37 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -12,14 +12,14 @@ app.get('/', function (req, res) {
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
-app.get('/article-one',function (req, res){
-  res.sendFile(path.join(__dirname, 'article-one.html'));
+app.get('/ui/article-one',function (req, res){
+  res.sendFile(path.join(__dirname,'ui', 'article-one.html'));
 });
-app.get('/article-two',function (req, res){
-      res.sendFile(path.join(__dirname,'article-two.html'));
+app.get('/ui/article-two',function (req, res){
+      res.sendFile(path.join(__dirname,'ui','article-two.html'));
 });
-app.get('/article-three',function (req, res){
-     res.sendFile(path.join(__dirname,'article-three.html'));
+app.get('/ui/article-three',function (req, res){
+     res.sendFile(path.join(__dirname,'ui','article-three.html'));
 });
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
